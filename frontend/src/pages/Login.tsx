ec049cf8c473d8afbf81484c6e7ef21683fd296b
ronaldllamas12 <ronaldllamas12@users.noreply.github.com>
2026-07-05 17:34:11 -0500

    update logo

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -73,14 +73,14 @@ export default function Login() {
         {/* Brand */}
         <div className="text-center mb-8">
           <div
-            className="inline-flex items-center justify-center w-16 h-16
+            className="inline-flex items-center justify-center w-20 h-20
                         bg-white rounded-2xl mb-5
                         shadow-xl shadow-blue-600 ring ring-blue-500"
           >
             <img
               src="/moto.png"
               alt="Logo"
-              className="w-14 h-14 object-contain"
+              className="w-18 h-18 object-contain"
             />
           </div>
           <h1 className="text-2xl font-bold text-white tracking-tight">Control de Acceso</h1>
